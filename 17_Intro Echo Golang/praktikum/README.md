2cd0b66e88a83a9d664a6201b377302cdd4e9934
Ariesta Heart <ariestaheart@gmail.com>
2022-09-26 17:19:44 +0700

    [Sec-17] Menambahkan praktikum

---
+++ 17_Intro Echo Golang/praktikum/README.md
@@ -0,0 +1,24 @@
+# PRAKTIKUM
+
+Diberikan praktikum sebagai berikut: https://docs.google.com/document/d/1Wp4i6ADwUZRB2fc1nHHLZJrA735l9EKuaPeiAk-DxUI/edit
+
+## Objective
+- Create Routing pada Echo
+- Create Controller pada Echo
+- Create Response pada Echo
+
+Buat Static API CRUD User dengan spesifikasi seperti berikut!
+- Pada bagian Sample Code artinya sudah disediakan contoh code yang bisa kamu implementasikan.
+- Pada bagian Need Solution Code kamu perlu membuat sendiri code-nya!
+
+|Route |HTTP|Description |Problem|
+|-|-|-|-|
+|/users|GET|Get all users data|Sample Code|
+|/users/:id|GET|Get single user by id|Need Solution Code|
+|/users|POST|Create new user|Sample Code|
+|/users/:id|DELETE|Delete user by id|Need Solution Code|
+|/users/:id|PUT|Update user information|Need Solution Code|
+
+## Jawaban
+
+Kode sumber jawaban ada pada berikut: [main.go](main.go)
---
+++ 17_Intro Echo Golang/praktikum/main.go
@@ -0,0 +1,138 @@
+package main
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/labstack/echo"
+)
+
+type User struct {
+	Id       int    `json:"id" form:"id"`
+	Name     string `json:"name" form:"name"`
+	Email    string `json:"email" form:"email"`
+	Password string `json:"password" form:"password"`
+}
+
+var users []User
+
+// -------------------- controller --------------------
+
+// get all users
+func GetUsersController(c echo.Context) error {
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"messages": "success get all users",
+		"users":    users,
+	})
+}
+
+// get user by id
+func GetUserController(c echo.Context) error {
+	// your solution here
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]interface{}{
+			"messages": "error",
+		})
+	}
+
+	for _, user := range(users) {
+		if user.Id == id {
+			return c.JSON(http.StatusOK, map[string]interface{}{
+				"messages": "success",
+				"users":    user,
+			})
+		}
+	}
+
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"messages": "user not exist",
+		"users": nil,
+	})
+}
+
+// delete user by id
+func DeleteUserController(c echo.Context) error {
+	// your solution here
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]interface{}{
+			"messages": "error",
+		})
+	}
+
+	for idx, user := range(users) {
+		if user.Id == id {
+			users = append(users[:idx], users[idx+1:]...)
+			return c.JSON(http.StatusOK, map[string]interface{}{
+				"messages": "success",
+			})
+		}
+	}
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"messages": "user not exist",
+	})
+}
+
+// update user by id
+func UpdateUserController(c echo.Context) error {
+	// your solution here
+
+	newUser := User{}
+	c.Bind(&newUser)
+
+	id, err := strconv.Atoi(c.Param("id"))
+	newUser.Id = id
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]interface{}{
+			"messages": "error",
+		})
+	}
+
+	for idx, user := range(users) {
+		if user.Id == id {
+			users[idx] = newUser
+			return c.JSON(http.StatusOK, map[string]interface{}{
+				"messages": "success",
+			})
+		}
+	}
+
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"messages": "user not exist",
+	})
+	return nil
+}
+
+// create new user
+func CreateUserController(c echo.Context) error {
+	// binding data
+	user := User{}
+	c.Bind(&user)
+
+	if len(users) == 0 {
+		user.Id = 1
+	} else {
+		newId := users[len(users)-1].Id + 1
+		user.Id = newId
+	}
+	users = append(users, user)
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"messages": "success create user",
+		"user":     user,
+	})
+}
+
+// ---------------------------------------------------
+func main() {
+	e := echo.New()
+	// routing with query parameter
+	e.GET("/users", GetUsersController)
+	e.POST("/users", CreateUserController)
+	e.DELETE("/users/:id", DeleteUserController)
+	e.GET("/users/:id", GetUserController)
+	e.PUT("/users/:id", UpdateUserController)
+
+	// start the server, and log if it fails
+	e.Logger.Fatal(e.Start(":8000"))
+}
